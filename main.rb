e811916600d8fcfe988163ee931f683790d402c1
Nick Isaacs <nisaacs@splickit.com>
2014-03-22 13:53:39 -0600

    ignore primes file, add cracking

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'fileutils'
 require 'set'
+# require 'debugger'
 
 module TimeExtensions
   Time.class_eval do
@@ -212,69 +213,137 @@ module BasicAssignment
     file_buffer = File.open file, 'r'
     mutex = Mutex.new
 
-    file_buffer.each_line.map{ |l| l.to_i }.each do |number|
-      result = nil
-      t = Time.measure do
+    file_buffer.each_line.map{ |l| l.to_i }.each_with_index do |number, index|
+      result = factor number
+      #puts "Factors of #{number} => #{result.inspect}\nTook #{t} seconds"
+      puts "Took #{t} seconds for #{number.to_s.size} digit number"
+    end
+  end
 
-        t1 = Thread.new do
-          q = @@seen_before.detect{ |i| number%i == 0 }
-          mutex.synchronize{ result = [q, number/q] }
-        end
+  def factor(number)
+    result = nil
+    t = Time.measure do
+      t1 = Thread.new do
+        q = @@seen_before.detect{ |i| number%i == 0 }
+        mutex.synchronize{ result = [q, number/q] }
+      end
 
-        t2 = Thread.new do
-          q = @@seen_before.reverse.detect{ |i| number%i == 0 }
-          mutex.synchronize{ result = [q, number/q] }
-        end
+      t2 = Thread.new do
+        q = @@seen_before.reverse.detect{ |i| number%i == 0 }
+        mutex.synchronize{ result = [q, number/q] }
+      end
 
-        t3 = Thread.new do
-          r = number.prime_factors
-          mutex.synchronize do
-            result = r
-            @@seen_before << result.first
-            @@seen_before << result[-1]
-          end
-        end
-
-        # Wait for one or the other to finish
-        begin
-          unless t1.status
-            t2.kill
-            t3.kill
-          end
-
-          unless t2.status
-            t1.kill
-            t3.kill
-          end
-
-          unless t3.status
-            t1.kill
-            t2.kill
-          end
-        end while (t1.status or t2.status or t3.status)
-
-        unless result
-          result = number.prime_factors
+      t3 = Thread.new do
+        r = number.prime_factors
+        mutex.synchronize do
+          result = r
           @@seen_before << result.first
           @@seen_before << result[-1]
         end
       end
 
-      puts "Factors of #{number} => #{result.inspect}\nTook #{t} seconds"
+      # Wait for one or the other to finish
+      begin
+        unless t1.status
+          t2.kill
+          t3.kill
+        end
+
+        unless t2.status
+          t1.kill
+          t3.kill
+        end
+
+        unless t3.status
+          t1.kill
+          t2.kill
+        end
+      end while (t1.status or t2.status or t3.status)
+
+      unless result
+        result = number.prime_factors
+        @@seen_before << result.first
+        @@seen_before << result[-1]
+      end
     end
+    result
+  end
+end
+
+module RSACrack
+  include BasicAssignment
+  include ExtraCredit
+
+  # message is an array of number strings, public key is an array [n, e]
+  def decode(msg, public_key)
+    r = []
+    d = break_pub_key public_key
+    n = public_key[0]
+
+    msg.each do |line|
+      line_int = line.to_i
+      decoded_line = (line**d)%n
+      puts decoded_line
+      r << decoded_line
+    end
+
+    # Unicode to str
+    str_arr = r.map{ |n| n.pack('U') }
+    puts str_arr.inject{ |acc, s| acc + "#{s} " }
   end
 
+  def break_pub_key(pub_key)
+    n = pub_key[0]
+    e = pub_key[1]
+
+    # Needed to find
+    p=q=k=v=d=nil;
+
+    p, q = BasicAssignment.factor n
+    k = (p-1) * (q-1)
+    compute_private_key k, e
+  end
+
+  def compute_private_key(k,e)
+    d,v,l = compute_GCD e, k
+
+    if ( d < 0)
+      m = (-d/k) + 1
+      d = d + m * k
+    end
+    d
+  end
+
+  def compute_GCD(u,v)
+    u1,u2,u3 = 1,0,u
+    v1,v2,v3 = 0,1,v
+    while (v3 > 0)
+      q = u3/v3
+      t1 = u1 - q * v1
+      t2 = u2 - q * v2
+      t3 = u3 - q * v3
+      u1,u2,u3 = v1,v2,v3
+      v1,v2,v3 = t1,t2,t3
+    end
+    [u1,u2,u3]
+  end
 end
 
 #----------- Main -------------
 
 include BasicAssignment
 include ExtraCredit
+include RSACrack
 
 file_name = ARGV.shift
 
 if ARGV.include? 'ec'
   ExtraCredit.execute file_name
+elsif file_name == 'crack'
+  # debugger
+  msg_arr = File.read(ARGV.shift).each_line.map{ |a| a.to_i }
+  pub_key = File.read(ARGV.shift).each_line.map{ |a| a.to_i }
+  RSACrack.decode msg_arr, pub_key
 else
   t = Time.measure{ BasicAssignment.do_it file_name }
   puts "\n\nWhole thing took #{t} seconds"
